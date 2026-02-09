fea3477400cb713cbd31b5da4ebf1e4286f4fb63
Hos9 <shahin9315@gmail.com>
2026-02-09 17:00:18 +0600

    Task-2 Done

--- practice-task2.js
+++ practice-task2.js
@@ -1,15 +1,30 @@
 console.log("Task-2");
+// Find the friend with the smallest name.
 
+const friends = [
+  "Sajid",
+  "Iqbal",
+  "Nasir",
+  "Rahi",
+  "Udoy",
+  "Fahim",
+  "Raj",
+  "Rayhan",
+];
 
+let smallestChar = friends[0].length;
+let smallestName = "";
 
+for (const i of friends) {
+  if (i.length < smallestChar) {
+    smallestChar = i.length;
+    smallestName = i;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
+console.log(
+  "Smallest Name Charecter is:",
+  smallestChar,
+  "and Smallest Name is:",
+  smallestName,
+);
