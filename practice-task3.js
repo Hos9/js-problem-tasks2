3206aab58171a11b2036d45b067b5d7e795bfcbb
Hos9 <shahin9315@gmail.com>
2026-02-09 17:05:25 +0600

    Task-3 Done

--- practice-task3.js
+++ practice-task3.js
@@ -1,21 +1,13 @@
 console.log("Task-3");
 
+function calculateElectronicsBudget(laptop, tables, mobile) {
+  const totalBudget = laptop + tables + mobile;
+  return totalBudget;
+}
 
+const laptop = 100000;
+const tables = 50000;
+const mobile = 30000;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const totalBudget = calculateElectronicsBudget(laptop, tables, mobile);
+console.log(totalBudget);
