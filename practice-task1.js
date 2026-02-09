77cc9caebb6f251fdbcb0c9d3bb706c69b8e726a
Hos9 <shahin9315@gmail.com>
2026-02-09 16:50:18 +0600

    Task1 done

--- practice-task1.js
+++ practice-task1.js
@@ -1,2 +1,11 @@
 console.log("Task-1");
 /* Find the lowest number in the array below. */
+const heights = [127, 167, 170, 178, 124, 165];
+
+let lowest = heights[0];
+for (const i of heights) {
+  if (i < lowest) {
+    lowest = i;
+  }
+}
+console.log(lowest);
