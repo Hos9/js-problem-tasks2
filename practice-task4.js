55006a1990bb7c4b4236f9f910c29d08bf3ba9d4
Hos9 <shahin9315@gmail.com>
2026-02-10 14:53:53 +0600

    Problem-4 1st attempt

--- practice-task4.js
+++ practice-task4.js
@@ -1,21 +1,20 @@
 console.log("Task-4");
 
+const phones = [
+  { model: "PhoneA", brand: "iPhone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
 
+function findAveragePhonePrice(phones) {
+  sum = 0;
+  for (const i of phone.price) {
+    let sum = sum + i;
+    return sum;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(findAveragePhonePrice(phones.price));
